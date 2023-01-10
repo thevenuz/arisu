0f8343a1c57701fec7b232a8b8349f005c1c6454
Sagar <67011477+thevenuz@users.noreply.github.com>
2023-01-10 23:46:14 +0530

    WIP: add responsiveness to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link} from "react-router-dom";
 import icon from '../assets/images/icon.png'
+import { PAGES } from '../enums/Pages';
 
 const Navbar = () => {
+
+  // State for the active tab
+  const [activePage, setActivePage] = useState(PAGES.HOME)
+
+  const setCurrentPage = (page) => {
+    setActivePage(page);
+  }
+
   return (
     <div className='flex w-full h-16 font-Outfit shadow-md'>
         <div className='flex w-4/5 mx-auto justify-center'>
@@ -11,11 +20,10 @@ const Navbar = () => {
                 <div className='align-middle w-1/3 px-2'>
                     <img className='w-14 h-14 rounded-full' src={icon} alt='icon'/>
                 </div>
-                <div className='align-middle font-bold text-lg leading-tight text-white'>
+                <div className='align-middle font-bold text-xl leading-tight text-white'>
                     <Link to="/">
                     <p>
-                        ARISU<br/>
-                        SAKAYANAGI
+                        ARISU-CAMP
                     </p>
                     </Link>
                         
@@ -26,27 +34,27 @@ const Navbar = () => {
             <div className='flex-1 items-center justify-end align-middle flex'>
                 <div className='flex space-x-4 align-middle px-2 font-medium text-white'>
                     <div className='align-middle'>
-                        <Link to="/" className="">
+                        <Link to="/" className={`align-middle ${activePage===PAGES.HOME ? 'text-accent drop-shadow-lg font-bold' : 'text-white'}`}  onClick={() => setCurrentPage(PAGES.HOME)}>
                             <p>Home</p>
                         </Link>
                     </div>
                     <div className='align-middle'>
-                        <Link to="/gallery" className="">
+                        <Link to="/gallery" className={`align-middle ${activePage===PAGES.GALLERY ? 'text-accent drop-shadow-lg font-bold' : 'text-white'}`} onClick={() => setCurrentPage(PAGES.GALLERY)}>
                             <p>Gallery</p>
                         </Link>
                     </div>
                     <div className='align-middle'>
-                        <Link to="/shortstories" className="">
+                        <Link to="/shortstories" className={`hover:text-accent ${activePage===PAGES.SHORTSTORIES ? 'text-accent' : 'text-white'}`} onClick={() => setCurrentPage(PAGES.SHORTSTORIES)}>
                             <p>Short Stories</p>
                         </Link>
                     </div>
                     <div className='align-middle'>
-                        <Link to="/fanfics" className="">
+                        <Link to="/fanfics" className={`hover:text-accent ${activePage===PAGES.FANFICS ? 'text-accent' : 'text-white'}`} onClick={() => setCurrentPage(PAGES.FANFICS)}>
                             <p>Fanfics</p>
                         </Link>
                     </div>
                     <div className='align-middle'>
-                        <Link to="/about" className="">
+                        <Link to="/about" className={`hover:text-accent ${activePage===PAGES.ABOUT ? 'text-accent' : 'text-white'}`} onClick={() => setCurrentPage(PAGES.ABOUT)}>
                             <p>About</p>
                         </Link>
                     </div>
---
+++ src/enums/Pages.js
@@ -0,0 +1,7 @@
+export const PAGES = {
+    HOME: "Home",
+    GALLERY: "Gallery",
+    SHORTSTORIES: "Shprt Stories",
+    FANFICS: "Fanfics",
+    ABOUT: "About"
+}
