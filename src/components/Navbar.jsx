f09176a5222a28dcfd7df2cf85742e87bde7296a
Sagar <67011477+thevenuz@users.noreply.github.com>
2023-01-14 00:22:24 +0530

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,16 +13,16 @@ const Navbar = () => {
   }
 
   return (
-    <div className='flex w-full h-16 font-Outfit shadow-md'>
+    <div className='flex w-full h-16 font-Heading shadow-md'>
         <div className='flex w-4/5 mx-auto justify-center'>
             {/* div for icon and title */}
-            <div className='flex items-center w-2/12 space-x-1'>
+            <div className='flex items-center w-2/12 space-x-1 '>
                 <div className='align-middle w-1/3 px-2'>
                     <img className='w-14 h-14 rounded-full' src={icon} alt='icon'/>
                 </div>
-                <div className='align-middle font-bold text-xl leading-tight text-white'>
+                <div className='align-middle font-bold text-xl leading-tight text-main-text'>
                     <Link to="/">
-                    <p>
+                    <p className='font-Heading'>
                         ARISU-CAMP
                     </p>
                     </Link>
@@ -32,29 +32,29 @@ const Navbar = () => {
 
             {/* div for about, gallery ss etc */}
             <div className='flex-1 items-center justify-end align-middle flex'>
-                <div className='flex space-x-4 align-middle px-2 font-medium text-white'>
+                <div className='flex space-x-4 align-middle px-2 text-base font-Heading font-bold text-main-text'>
                     <div className='align-middle'>
-                        <Link to="/" className={`align-middle ${activePage===PAGES.HOME ? 'text-accent drop-shadow-lg font-bold' : 'text-white'}`}  onClick={() => setCurrentPage(PAGES.HOME)}>
+                        <Link to="/" className={`align-middle ${activePage===PAGES.HOME ? 'underline underline-offset-8 decoration-3 decoration-accent-bar' : ' '}`}  onClick={() => setCurrentPage(PAGES.HOME)}>
                             <p>Home</p>
                         </Link>
                     </div>
                     <div className='align-middle'>
-                        <Link to="/gallery" className={`align-middle ${activePage===PAGES.GALLERY ? 'text-accent drop-shadow-lg font-bold' : 'text-white'}`} onClick={() => setCurrentPage(PAGES.GALLERY)}>
+                        <Link to="/gallery" className={`align-middle ${activePage===PAGES.GALLERY ? 'underline underline-offset-8 decoration-3 decoration-accent-bar' : ''}`} onClick={() => setCurrentPage(PAGES.GALLERY)}>
                             <p>Gallery</p>
                         </Link>
                     </div>
                     <div className='align-middle'>
-                        <Link to="/shortstories" className={`hover:text-accent ${activePage===PAGES.SHORTSTORIES ? 'text-accent' : 'text-white'}`} onClick={() => setCurrentPage(PAGES.SHORTSTORIES)}>
+                        <Link to="/shortstories" className={`hover:text-accent ${activePage===PAGES.SHORTSTORIES ? 'underline underline-offset-8 decoration-3 decoration-accent-bar' : ''}`} onClick={() => setCurrentPage(PAGES.SHORTSTORIES)}>
                             <p>Short Stories</p>
                         </Link>
                     </div>
                     <div className='align-middle'>
-                        <Link to="/fanfics" className={`hover:text-accent ${activePage===PAGES.FANFICS ? 'text-accent' : 'text-white'}`} onClick={() => setCurrentPage(PAGES.FANFICS)}>
+                        <Link to="/fanfics" className={`hover:text-accent ${activePage===PAGES.FANFICS ? 'underline underline-offset-8 decoration-3 decoration-accent-bar' : ''}`} onClick={() => setCurrentPage(PAGES.FANFICS)}>
                             <p>Fanfics</p>
                         </Link>
                     </div>
                     <div className='align-middle'>
-                        <Link to="/about" className={`hover:text-accent ${activePage===PAGES.ABOUT ? 'text-accent' : 'text-white'}`} onClick={() => setCurrentPage(PAGES.ABOUT)}>
+                        <Link to="/about" className={`hover:text-accent ${activePage===PAGES.ABOUT ? 'underline underline-offset-8 decoration-3 decoration-accent-bar' : ''}`} onClick={() => setCurrentPage(PAGES.ABOUT)}>
                             <p>About</p>
                         </Link>
                     </div>
