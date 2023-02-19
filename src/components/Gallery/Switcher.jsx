c017e6c8e3d6108a45233555ff976d28ca4a4ad0
Sagar <67011477+thevenuz@users.noreply.github.com>
2023-02-19 20:32:46 +0530

    add logic to preserve state of current active gallery tab

--- src/components/Gallery/Switcher.jsx
+++ src/components/Gallery/Switcher.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { IMAGETYPES } from '../../enums/ImageType';
 
-const Switcher = ({activeTab, setActiveTab}) => {
+const Switcher = ({activeTab, setActiveTab, setUrlParams}) => {
 
     // Set Active tab state
     const setCurrentTab = (imageType) => {
@@ -13,17 +13,29 @@ const Switcher = ({activeTab, setActiveTab}) => {
     <div className='flex w-full items-center align-middle my-6 font-Heading'>
         <div className='w-1/3 h-10 rounded-lg flex mx-auto shadow-md font-bold text-main-text border-[1px] border-main-bg'>
             {/* Different Image sources */}
-            <div className={`flex-auto flex justify-center align-middle items-center shadow-md ${activeTab === IMAGETYPES.LNIllustrations ? ' bg-accent-bar rounded-lg' : ''}`} onClick={() => setCurrentTab(IMAGETYPES.LNIllustrations)}>
+            <div className={`flex-auto flex justify-center align-middle items-center shadow-md ${activeTab === IMAGETYPES.LNIllustrations ? ' bg-accent-bar rounded-lg' : ''}`} onClick={() => {
+              setCurrentTab(IMAGETYPES.LNIllustrations);
+              setUrlParams({'tab':'ln'});
+              }}>
                 <a href='#'>LN Illustrations</a>
             </div>
-            <div className={`flex-auto flex justify-center align-middle items-center shadow-md ${activeTab === IMAGETYPES.OfficialArt ? 'bg-accent-bar rounded-lg' : ''}`} onClick={() => setCurrentTab(IMAGETYPES.OfficialArt)}>
+            <div className={`flex-auto flex justify-center align-middle items-center shadow-md ${activeTab === IMAGETYPES.OfficialArt ? 'bg-accent-bar rounded-lg' : ''}`} onClick={() => {
+              setCurrentTab(IMAGETYPES.OfficialArt);
+              setUrlParams({'tab':'off-art'});
+              }}>
                 <a href='#'>Official Art</a>
             </div>
-            <div className={`flex-auto flex justify-center align-middle items-center shadow-md ${activeTab === IMAGETYPES.Fanart ? 'bg-accent-bar rounded-lg' : ''}`}  onClick={() => setCurrentTab(IMAGETYPES.Fanart)}>
-                <a href='#'>Fanart</a>
+            <div className={`flex-auto flex justify-center align-middle items-center shadow-md ${activeTab === IMAGETYPES.Fanart ? 'bg-accent-bar rounded-lg' : ''}`}  onClick={() => {
+              setCurrentTab(IMAGETYPES.Fanart);
+              setUrlParams({'tab':'fanart'});
+              }}>
+              <a href='#'>Fanart</a>
             </div>
-            <div className={`flex-auto flex justify-center align-middle items-center shadow-md ${activeTab === IMAGETYPES.AIArt ? 'bg-accent-bar rounded-lg' : ''}`}  onClick={() => setCurrentTab(IMAGETYPES.AIArt)}>
-                <a href='#'>AI Art</a>
+            <div className={`flex-auto flex justify-center align-middle items-center shadow-md ${activeTab === IMAGETYPES.AIArt ? 'bg-accent-bar rounded-lg' : ''}`}  onClick={() => {
+              setCurrentTab(IMAGETYPES.AIArt);
+              setUrlParams({'tab':'ai'});
+              }}>
+              <a href='#'>AI Art</a>
             </div>
         </div>
     </div>
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,21 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Footer from '../components/Footer';
 import Images from '../components/Gallery/Images';
 import Switcher from '../components/Gallery/Switcher';
 import { IMAGETYPES } from '../enums/ImageType';
+import {useSearchParams} from 'react-router-dom';
 
 const Gallery = () => {
 
   // State
   const [activeTab, setActiveTab] = useState(IMAGETYPES.LNIllustrations);
+  const [urlParams, setUrlParams] = useSearchParams();
+
+  const [queryTab] = useState(urlParams.get('tab'));
+
+  useEffect(()=>{
+    if(queryTab){
+      switch(queryTab){
+        case 'ln':
+          setActiveTab(IMAGETYPES.LNIllustrations);
+          break;
+        case 'off-art':
+          setActiveTab(IMAGETYPES.OfficialArt);
+          break;
+        case 'fanart':
+          setActiveTab(IMAGETYPES.Fanart);
+          break;
+        case 'ai':
+          setActiveTab(IMAGETYPES.AIArt);
+          break;
+        default:
+          setActiveTab(IMAGETYPES.LNIllustrations);
+          break;
+      }
+    }
+  }, []);
 
   return (
     <div className='flex container w-full overflow-y-hidden'>
+      <div className='w-full mx-auto overflow-y-auto no-scrollbar'>
         <div className='w-4/5 mx-auto flex-col overflow-y-auto no-scrollbar'>
-            <Switcher activeTab = {activeTab} setActiveTab= {setActiveTab}/>
+            <Switcher activeTab = {activeTab} setActiveTab= {setActiveTab} setUrlParams={setUrlParams}/>
             <Images activeTab = {activeTab}/>
             <Footer/> 
         </div>  
+        </div>
     </div>
   )
 }
