0326818963406208adbbe6cce9effb4928241153
Sagar <67011477+thevenuz@users.noreply.github.com>
2023-03-03 23:14:06 +0530

    update tailwind config with custom config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,39 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'Outfit': ['Outfit', 'sans-serif']
+        'SS-content': ['Source Sans Pro', 'sans-serif'],
+        'Heading' : ['Figtree', 'sans-serif'],
+        'Content' : ['Source Sans Pro', 'sans-serif'],
       },
       blur: {
         xs: '0.5px',
       },
+      colors: {
+        'main-bg': '#9FA1DE',
+        'main-text': '#1A2657',
+        'accent-bar': '#6BFBCE',
+        'gradient-from': 'rgba(222,165,204,1)',
+        'gradient-to': 'rgba(159,161,222,1)'
+      },
+      textUnderlineOffset: {
+        3: '3px',
+      },
+      textDecorationThickness: {
+        3: '2.5px',
+      },
+      height: {
+        '2.5': '2.5px',
+      },
+      keyframes: {
+        'open-menu':{
+          '0%': {transform: 'scaleY(0)'},
+          '80%': {transform: 'scaleY(1.2)'},
+          '100%': {transform: 'scaleY(1)'},
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      }
     },
   },
   plugins: [],
