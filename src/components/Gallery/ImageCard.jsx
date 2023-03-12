a6ca4dac43f0d0f3dbf0b794b08c560c9d469211
Sagar <67011477+thevenuz@users.noreply.github.com>
2023-03-12 22:55:21 +0530

    add nsfw warning for explicit image links

--- src/components/Gallery/ImageCard.jsx
+++ src/components/Gallery/ImageCard.jsx
@@ -4,14 +4,15 @@ import {LazyLoadImage } from 'react-lazy-load-image-component';
 const ImageCard = ({image}) => {
   return (
     <div className=' break-inside-avoid'>
-        <figure className='mb-1 p-1 hover:p-0'>
+        <figure className='mb-1 p-1 cursor-pointer'>
           <img className='rounded-t-md' src={image.ImagePath} alt={image.Title} loading='lazy'/>
           {/* <LazyLoadImage className='rounded-t-md' src={image.ImagePath} alt={image.Title} loading='lazy'/> */}
-          <figcaption className='shadow-lg rounded-b-md p-1 w-full font-Content'>
+          <figcaption className='shadow-lg rounded-b-md mt-1 p-1 w-full font-Content'>
             <p className='px-1 text-xs md:text-sm drop-shadow-md'>
-              <span className='font-bold'>{image.Title}</span>
-              <span className='italic font-semibold'>{` by ${image.Creator} - `}</span>
-              <a className='underline underline-offset-2 decoration-3 font-medium hover:decoration-accent-bar hover:text-lg hover:font-bold' href={image.Source} target='_blank'>source</a>
+              <span className='font-bold hover:opacity-90'>{image.Title}</span>
+              <span className='italic font-semibold hover:opacity-90'>{` by ${image.Creator} - `}</span>
+              <a className='underline underline-offset-2 decoration-3 font-medium hover:decoration-accent-bar hover:opacity-90' href={image.Source} target='_blank'>source</a>
+              {image.IsNsfw ? <span title='NSFW warning: Source link can redirect to a page with possibily explicit content' className=' bg-black text-red-500 ml-2 mt-1 font-extrabold text-xs p-1 rounded-md cursor-pointer'>NSFW</span> : ''}
             </p>
           </figcaption>
         </figure>
