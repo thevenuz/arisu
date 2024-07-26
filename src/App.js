3a1d605a25482b1b5a1b5b6c3db9813212e8d31e
Sagar <67011477+thevenuz@users.noreply.github.com>
2024-07-25 21:36:17 -0500

    add new ln vols art

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from './pages/Home';
 import Navbar from './components/Shared/Navbar';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Gallery from './pages/Gallery';
 import ShortStories from './pages/ShortStories';
 import ShortStoryContent from './components/ShortStories/ShortStoryContent';
@@ -11,41 +11,98 @@ import { useEffect } from 'react';
 import { PAGES } from './enums/Pages';
 
 function App() {
-
   const [activePage, setActivePage] = useState(PAGES.HOME);
   const [isMobileNavbarOpen, setIsMobileNavbarOpen] = useState(() => false);
-  const [isDarkMode, setIsDarkMode] = useState(()=>{
+  const [isDarkMode, setIsDarkMode] = useState(() => {
     const darkModeFromStorage = localStorage.getItem('DarkMode');
 
-    return darkModeFromStorage !== null ? JSON.parse(darkModeFromStorage) : false;
+    return darkModeFromStorage !== null
+      ? JSON.parse(darkModeFromStorage)
+      : false;
   });
 
   // Save Dark Mode/Light Mode in local storage
 
   useEffect(() => {
     localStorage.setItem('DarkMode', JSON.stringify(isDarkMode));
-  }, [isDarkMode])
+  }, [isDarkMode]);
 
   return (
-    <div className={`App flex flex-col h-screen 
-      ${isDarkMode ? 'bg-main-text text-slate-50' : 
-      'bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gradient-from via-gradient-to to-gradient-from' }
+    <div
+      className={`App flex flex-col h-screen 
+      ${
+        isDarkMode
+          ? 'bg-main-text text-slate-50'
+          : 'bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gradient-from via-gradient-to to-gradient-from'
+      }
     `}>
-    
-    <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} activePage={activePage} setActivePage={setActivePage} isMobileNavbarOpen={isMobileNavbarOpen}
-    setIsMobileNavbarOpen={setIsMobileNavbarOpen} />
-    
-    <Routes>
-        <Route path="/" element={<Home setActivePage={setActivePage} isMobileNavbarOpen={isMobileNavbarOpen}/>}/>
-        <Route path="/gallery" element={<Gallery isDarkMode={isDarkMode} setActivePage={setActivePage} isMobileNavbarOpen={isMobileNavbarOpen}/>} />
-        <Route path="/shortstories" element={<ShortStories setActivePage={setActivePage} isMobileNavbarOpen={isMobileNavbarOpen}/>}/>
-        <Route path="/ss/:name" element={<ShortStoryContent setActivePage={setActivePage} isMobileNavbarOpen={isMobileNavbarOpen}/>}/>
-        <Route path="/fanfics" element={<Fanfics setActivePage={setActivePage} isMobileNavbarOpen={isMobileNavbarOpen}/>}/>
-        <Route path="/about" element={<About setActivePage={setActivePage} isMobileNavbarOpen={isMobileNavbarOpen}/>}/>
-    </Routes>
-    
+      <Navbar
+        isDarkMode={isDarkMode}
+        setIsDarkMode={setIsDarkMode}
+        activePage={activePage}
+        setActivePage={setActivePage}
+        isMobileNavbarOpen={isMobileNavbarOpen}
+        setIsMobileNavbarOpen={setIsMobileNavbarOpen}
+      />
+
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <Home
+              setActivePage={setActivePage}
+              isMobileNavbarOpen={isMobileNavbarOpen}
+            />
+          }
+        />
+        <Route
+          path='/gallery'
+          element={
+            <Gallery
+              isDarkMode={isDarkMode}
+              setActivePage={setActivePage}
+              isMobileNavbarOpen={isMobileNavbarOpen}
+            />
+          }
+        />
+        <Route
+          path='/shortstories'
+          element={
+            <ShortStories
+              setActivePage={setActivePage}
+              isMobileNavbarOpen={isMobileNavbarOpen}
+            />
+          }
+        />
+        <Route
+          path='/ss/:name'
+          element={
+            <ShortStoryContent
+              setActivePage={setActivePage}
+              isMobileNavbarOpen={isMobileNavbarOpen}
+            />
+          }
+        />
+        <Route
+          path='/fanfics'
+          element={
+            <Fanfics
+              setActivePage={setActivePage}
+              isMobileNavbarOpen={isMobileNavbarOpen}
+            />
+          }
+        />
+        <Route
+          path='/about'
+          element={
+            <About
+              setActivePage={setActivePage}
+              isMobileNavbarOpen={isMobileNavbarOpen}
+            />
+          }
+        />
+      </Routes>
     </div>
-    
   );
 }
 
